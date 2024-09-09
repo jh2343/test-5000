ec056b80f4b123165ec002c64d8302f69edd0af9
jh2343 <jh2343@georgetown.edu>
2024-09-09 14:41:33 -0400

    ive updated the shell script

--- git-sync.sh
+++ git-sync.sh
@@ -1,6 +1,29 @@
-git pull 
+printf 'Would you like to sync with the github server: (y/n)?'
+read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then 
 
-echo "SYNCING TO CLOUD"
-git add ./
-git commit -m "pushing to git"
-git push
+    # PULL CLOUD REPO TO LOCAL
+    git pull 
+    
+    # change buffer settings 
+    git config http.postBuffer 524288000
+
+    # SYNC TO LOCAL REPO TO CLOUD 
+    read -p 'ENTER MESSAGE: ' message
+    echo "commit message = "$message; 
+    
+    # ADD CHANGES TO QUEUE
+    git add ./; 
+    
+    # MAIN BRANCH
+    git commit -m "$message"; 
+
+    # PUSH NON-MAIN BRANCH
+    # git push  -u origin branch_name
+
+    # PUSH MAIN BRANCH
+    git push
+
+else
+    echo NOT SYNCING TO GITHUB!
+fi
